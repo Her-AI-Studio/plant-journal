2c4bb02a9a2ce25e5d349057b0be84fc8e501a9e
Jen Looper <jen.looper@gmail.com>
2026-06-16 18:48:32 -0400

    adding a base folder

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   root: '.',
   server: { port: 5174, open: true },
+  base: '/plant-journal/',
   optimizeDeps: {
     exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util'],
   },
